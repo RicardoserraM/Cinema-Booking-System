9912ca10a84a97b9fe560544a94caa8c901de41a
Ricardo Serra <ricardoserra19nsl@gmail.com>
2026-05-27 23:48:14 +0100

    feat: REFACTORIZACION EN ARCHIVO CSS , OPTIMIZACION DE CODIGO. ARCHIVOS / FOLDERS NUEVAS: FOLDER STYLES, BASE, COMPONENTS, PAGES, ARVHIVOS NUEVOS: vite.config y reserve-section

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        reserve: resolve(__dirname, 'reserve-section.html'),
+      },
+    },
+  },
+});
